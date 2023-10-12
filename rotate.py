7eabb6682cfa561ce4fff1eb630b700f3c5e4ab3
Thanathat Pinthu <thanathatp2003@gmail.com>
2023-10-12 11:22:02 +0700

    convert to function

--- rotate.py
+++ rotate.py
@@ -1,43 +1,38 @@
 import cv2
 import numpy as np
 
-angle = int(input("Enter Rotation Angle (Degrees) : "))
+def rotate_image(img, angle):
+    img = cv2.imread(f'imgin/{img}')
+    h, w = img.shape[:2] # return (height, width, chanel)
+    center_x, center_y = w // 2, h // 2
 
-img = cv2.imread('imgin/fox.jpg')
-h, w = img.shape[:2] # return (height, width, chanel)
-center_x, center_y = w // 2, h // 2
+    # Generate Transformation Matrix
+    rotation_matrix = cv2.getRotationMatrix2D((center_x, center_y), angle, 1.0)
+    cos_of_rotation_matrix = np.abs(rotation_matrix[0][0])
+    sin_of_rotation_matrix = np.abs(rotation_matrix[0][1])
 
-# Generate Transformation Matrix
-rotation_matrix = cv2.getRotationMatrix2D((center_x, center_y), angle, 1.0)
-cos_of_rotation_matrix = np.abs(rotation_matrix[0][0])
-sin_of_rotation_matrix = np.abs(rotation_matrix[0][1])
+    # Compute new size of an image
+    new_w = int((h * sin_of_rotation_matrix) + (w * cos_of_rotation_matrix))
+    new_h = int((h * cos_of_rotation_matrix) + (w * sin_of_rotation_matrix))
+    new_center_x, new_center_y = new_w / 2, new_h / 2
 
-# Compute new size of an image
-new_w = int((h * sin_of_rotation_matrix) + (w * cos_of_rotation_matrix))
-new_h = int((h * cos_of_rotation_matrix) + (w * sin_of_rotation_matrix))
-new_center_x, new_center_y = new_w / 2, new_h / 2
+    # Update Transformation Matrix New Center (x,y)
+    rotation_matrix[0][2] += new_center_x - center_x
+    rotation_matrix[1][2] += new_center_y - center_y
 
-# Update Transformation Matrix New Center (x,y)
-rotation_matrix[0][2] += new_center_x - center_x
-rotation_matrix[1][2] += new_center_y - center_y
+    rotated_img = cv2.warpAffine(img, rotation_matrix, (new_w, new_h))
 
-rotated_img = cv2.warpAffine(img, rotation_matrix, (new_w, new_h))
+    # Detect Image Contour and Crop an image
+    gray = cv2.cvtColor(rotated_img, cv2.COLOR_BGR2GRAY)
+    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    x_min, y_min, x_max, y_max = float('inf'), float('inf'), -float('inf'), -float('inf')
 
-# Detect Image Contour and Crop an image
-gray = cv2.cvtColor(rotated_img, cv2.COLOR_BGR2GRAY)
-contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-x_min, y_min, x_max, y_max = float('inf'), float('inf'), -float('inf'), -float('inf')
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        x_min = min(x_min, x)
+        y_min = min(y_min, y)
+        x_max = max(x_max, x + w)
+        y_max = max(y_max, y + h)
 
-for contour in contours:
-    x, y, w, h = cv2.boundingRect(contour)
-    x_min = min(x_min, x)
-    y_min = min(y_min, y)
-    x_max = max(x_max, x + w)
-    y_max = max(y_max, y + h)
-
-cropped_image = rotated_img[y_min:y_max, x_min:x_max]
-
-cv2.imwrite('imgout/rotated.png', cropped_image)
-cv2.imshow('rotated1.png', cropped_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cropped_image = rotated_img[y_min:y_max, x_min:x_max]
+    return cropped_image
