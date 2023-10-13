6bf1d1ee4f4946f98e0e44897d9a1ebe420d5f06
Racha Tungtrakool <65010902@kmitl.ac.th>
2023-10-13 22:31:34 +0700

    add function color_threshold

--- color_threshold.py
+++ color_threshold.py
@@ -1,44 +1,56 @@
 import matplotlib.pyplot as plt
-from matplotlib.colors import hsv_to_rgb
 import numpy as np
-np.set_printoptions(threshold=np.inf)
 import cv2
 
-# flags = [i for i in dir(cv2) if i.startswith("COLOR_")]    
-# print(flags)
+def color_threshold(img_inp):
 
+    # get input about user lower and upper thredhold   (RGB)
+    lower_threshold = [int(x) for x in input("Enter LOWER Threshold (RGB) ex. xxx xxx xxx : ").split(' ')]
+    upper_threshold = [int(x) for x in input("Enter UPPER Threshold (RGB) ex. xxx xxx xxx : ").split(' ')]
 
-nemo = cv2.imread("images/nemo0.jpg")
-
-nemo_rgb = cv2.cvtColor(nemo, cv2.COLOR_BGR2RGB)
-nemo_hsv = cv2.cvtColor(nemo, cv2.COLOR_BGR2HSV)
-
-
-low_white = np.uint8([[[210, 209, 209]]])
-high_white = np.uint8([[[255, 255, 255]]])
-
-hsv_low_white = cv2.cvtColor(low_white, cv2.COLOR_RGB2HSV)
-hsv_high_white = cv2.cvtColor(high_white, cv2.COLOR_RGB2HSV)
+    lower = (lower_threshold[0], lower_threshold[1], lower_threshold[2])
+    higher = (upper_threshold[1], upper_threshold[1], upper_threshold[2])
 
 
 
-light_orange = (1, 190, 200)
+    img_final = img_inp.copy()
 
-dark_orange = (18, 255, 255)
+    # make mask from colot threshold  
+    mask = cv2.inRange(img_inp, lower, higher)
 
-
-mask = cv2.inRange(nemo_hsv, hsv_low_white, hsv_high_white)
-
-cv2.imshow("mask", mask)
-
+    # convert from GRAT TO RGB because it will be mask that have only black and wise
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
 
 
-indice = np.where(mask == 255)
+    # make img to HSV beceasue it can change color more natural   ( tint )
+    img_final_hsv = cv2.cvtColor(img_final, cv2.COLOR_RGB2HSV)
 
-nemo_rgb[indice[0], indice[1], :] = [0, 255, 0]
+    user_desire_color = int(input("input color that you want (HUE)  0 - 255 : "))
+
+    for y in range(mask.shape[0]):
+        for x in range(mask.shape[1]):
+                if mask[y, x][0] == 255 and mask[y, x][1] == 255 and mask[y, x][2] == 255:
+                    img_final_hsv[y, x][0] = user_desire_color
+
+    img_final_hsv_rgb = cv2.cvtColor(img_final_hsv, cv2.COLOR_HSV2RGB)
 
 
-cv2.waitKey(0)
+    return img_final_hsv_rgb
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
 
 
 
