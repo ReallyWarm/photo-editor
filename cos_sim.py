7ff847286cd0a3e135cad82bba167cfe6e2a50c1
Racha Tungtrakool <65010902@kmitl.ac.th>
2023-10-18 11:23:49 +0700

    fix padding function case ODD EVEN

--- cos_sim.py
+++ cos_sim.py
@@ -2,7 +2,15 @@ from all_import import *
 
 def padding0(edited_image, vert_padding, hori_padding):
         # Pad the resized image with zeros
-        resized_padded = cv2.copyMakeBorder(edited_image, vert_padding // 2, vert_padding // 2, hori_padding // 2, hori_padding // 2, cv2.BORDER_CONSTANT, None, value=0)
+        if vert_padding % 2 == 1 and hori_padding % 2 == 1:               # vertical ODD, Horizon is ODD
+              resized_padded = cv2.copyMakeBorder(edited_image, vert_padding // 2 + 1, vert_padding // 2, hori_padding // 2 + 1, hori_padding // 2, cv2.BORDER_CONSTANT, None, value=0)
+        elif vert_padding % 2 == 1 and hori_padding % 2 == 0:               # vertical ODD , Horizon Even
+                resized_padded = cv2.copyMakeBorder(edited_image, vert_padding // 2 + 1, vert_padding // 2, hori_padding // 2, hori_padding // 2, cv2.BORDER_CONSTANT, None, value=0)
+        elif vert_padding % 2 == 0 and hori_padding % 2 == 1:               # vertical EVEN , Horizon is ODD
+                resized_padded = cv2.copyMakeBorder(edited_image, vert_padding // 2, vert_padding // 2, hori_padding // 2 + 1, hori_padding // 2, cv2.BORDER_CONSTANT, None, value=0)
+        else:                                                               # vertiacl EVEN , Horizon EVEN
+              resized_padded = cv2.copyMakeBorder(edited_image, vert_padding // 2, vert_padding // 2, hori_padding // 2, hori_padding // 2, cv2.BORDER_CONSTANT, None, value=0)
+               
         return resized_padded
 
 def cosine_similarity(vector1, vector2):
